55da2266ddcb2f4843b042763618b64ef8f390c0
Ruben Voss <bekertshop@gmail.com>
2022-08-17 16:45:26 +0200

    added frontend

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,8 @@
 class ReviewsController < ApplicationController
+  def show
+    @review = Review.find(params[:id])
+  end
+
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
