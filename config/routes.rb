f89f42785e34679c71d794cbf6b03b6002cc1109
Ruben Voss <bekertshop@gmail.com>
2022-08-17 15:08:14 +0200

    added restaurants-controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "restaurants", to: "restaurants#index"
+  # get "restaurants", to: "restaurants#index"
   # get "restaurants/:id", to: "restaurant#show"
+  resources :restaurants
 end
