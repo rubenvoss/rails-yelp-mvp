320ec11d0d112ddc1c2681ef63f244dfbf51b71f
Ruben Voss <bekertshop@gmail.com>
2022-08-17 16:00:55 +0200

    added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  restaurant = Restaurant.create(name: "rezi", address: "lul", phone_number: "235", category: "chinese")
+  Review.create(restaurant_id: restaurant[:id], content: "lul", rating: 2)
+end
