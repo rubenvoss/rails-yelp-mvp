4bf7d0756258a3801ca0ed53cc58952aecd394d8
Ruben Voss <bekertshop@gmail.com>
2022-08-17 16:08:24 +0200

    added the connection between restaurant and review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,9 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
+    @review.restaurant = @restaurant
     if @review.save
       @review.save
       redirect_to restaurant_path(@restaurant)
