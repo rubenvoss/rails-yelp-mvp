6fc80a1b6e48dcee8242af56fa8f84ebfe766dae
Ruben Voss <bekertshop@gmail.com>
2022-08-17 14:45:07 +0200

    fixed review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, :rating, presence: true
-  # validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: (0..5) }
 end
